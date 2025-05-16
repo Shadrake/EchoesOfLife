4c7a3a0fe32927c2dea34c73350a72b951b84b9a
ShadrakeAG <abril.goico@gmail.com>
2025-05-16 17:17:07 +0200

    add controls canvas in main menu

--- EchoesOfLife_AbrilGoicoecheaProj2/Assets/AGG/Scripts/GameManager.cs
+++ EchoesOfLife_AbrilGoicoecheaProj2/Assets/AGG/Scripts/GameManager.cs
@@ -7,10 +7,13 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject controlsUI;
+
     public void Lvl1Button()
     {
         SceneManager.LoadScene("scn_Lvl1");
     }
+
     public void MainMenuButton()
     {
         SceneManager.LoadScene("scn_MainMenu");
@@ -19,5 +22,15 @@ public class GameManager : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
+    }
+    
+    public void OpenControls()
+    {
+        controlsUI.SetActive(true);
+    }
+
+    public void CloseControls()
+    {
+        controlsUI.SetActive(false);
     }
 }

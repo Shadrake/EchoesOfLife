99fd292a65ab931b0e14896a5a38e80b9a8a67e7
ShadrakeAG <abril.goico@gmail.com>
2025-05-16 17:41:34 +0200

    add pause and controls menu, but when reseting from pause character won't move

--- EchoesOfLife_AbrilGoicoecheaProj2/Assets/AGG/Scripts/GameManager.cs
+++ EchoesOfLife_AbrilGoicoecheaProj2/Assets/AGG/Scripts/GameManager.cs
@@ -8,6 +8,66 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public GameObject controlsUI;
+    public GameObject pauseUI;
+    public GameObject player;  
+    private bool isPaused = false; 
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPaused)
+            {
+                OpenPause();
+            }
+            else
+            {
+                ClosePause();
+            }
+        }
+    }
+
+    public void OpenPause()
+    {
+        pauseUI.SetActive(true); 
+        Time.timeScale = 0f;
+        isPaused = true; 
+
+        if (player != null)
+        {
+            var playerController = player.GetComponent<PlayerController>();
+            if (playerController != null)
+            {
+                playerController.enabled = false;  
+            }
+        }
+    }
+
+    public void ClosePause()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false; 
+
+        if (player != null)
+        {
+
+            var playerController = player.GetComponent<PlayerController>();
+            if (playerController != null)
+            {
+                playerController.enabled = true; 
+            }
+        }
+    }
+    public void OpenControls()
+    {
+        controlsUI.SetActive(true); 
+    }
+
+    public void CloseControls()
+    {
+        controlsUI.SetActive(false);
+    }
 
     public void Lvl1Button()
     {
@@ -22,15 +82,5 @@ public class GameManager : MonoBehaviour
     public void Exit()
     {
         Application.Quit();
-    }
-    
-    public void OpenControls()
-    {
-        controlsUI.SetActive(true);
-    }
-
-    public void CloseControls()
-    {
-        controlsUI.SetActive(false);
     }
 }
